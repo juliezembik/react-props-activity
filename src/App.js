362bf18bc58dc40b7e3179a1924f6525be5c5b69
Julie Zembik <kurokurosan@gmail.com>
2019-01-16 15:32:50 -0600

    added enternumber to page. Add buttons, number does not show up yet. added css to enternumber.js. Connected to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import EnterNumber from './EnterNumber';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
             <Header />
+            <EnterNumber />
       </div>
     );
   }
---
+++ src/EnterNumber.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './EnterNumber.css';
+
+class EnterNumber extends Component {
+    render() {
+        return (
+            <div>
+                <button>Up</button>
+                    <br />
+                    <p>Number shows up here.</p>
+                    <br />
+                <button>Down</button>
+            </div>
+        );
+    }
+}
+
+
+
+
+export default EnterNumber;
