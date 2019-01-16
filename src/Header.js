68cf4d9b1ad75997fdedaf3e5b4c7fc76915864a
Julie Zembik <kurokurosan@gmail.com>
2019-01-16 15:13:46 -0600

    created header and header.css, added basic header and header css

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header>React: Props Activity</header>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+export default Header;
